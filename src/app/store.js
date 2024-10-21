faf9e160b1ba69b9f7b645fe7d4a32b469940b58
Addy Rodriguez <rdz.33.a@gmail.com>
2024-10-21 15:05:19 -0500

    part 1

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import postsReducer from '../features/posts/postSlice.js'
+
+export default configureStore({
+  reducer: {
+    posts: postsReducer
+  } 
+})
