b108df1ddd4ef95617a42f5b90584c8881d3f58f
Addy Rodriguez <rdz.33.a@gmail.com>
2024-10-22 12:38:33 -0500

    part2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import {
 } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
+
 import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
+import { EditPostForm } from './features/posts/EditPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 
 function App() {
   return (
@@ -20,12 +23,14 @@ function App() {
             exact
             path="/"
             render={() => (
-              <>
-              <AddPostForm/>
-                <PostsList/>
-              </>
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
